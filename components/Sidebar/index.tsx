346b62155cc1c2cae34045407924823ac1eda374
ChriChan <1198599982@qq.com>
2020-07-16 10:43:44 +0800

    update sidebar links' navigation target

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -6,13 +6,13 @@ export default () => (
   <div className={style.sidebar}>
     <span>·&nbsp;友情链接&nbsp;·</span>
     <span>
-      <a href="http://eps.ink">eps.ink</a>
+      <a href="http://eps.ink" target="_new">eps.ink</a>
     </span>
     <hr />
     <span>©2020 ChrisChan</span>
     <span>
       Powered&nbsp;by&nbsp;
-      <a href="https://github.com/ChrisChan13">@ChrisChan13</a>
+      <a href="https://github.com/ChrisChan13" target="_new">@ChrisChan13</a>
     </span>
     <span>联系我&nbsp;:&nbsp;chrischan1213@qq.com</span>
   </div>
